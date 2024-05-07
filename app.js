912be39ad8ac8ac9cf8f7667cb93ccd4dd6eb671
Ashrafu <ashraftuyubahe001@gmail.com>
2024-05-07 22:01:22 +0200

    making swagger for posturl api

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ const db = require("./models/dbconnection");
 const shortid = require("short-id");
 const jwt= require('jsonwebtoken');
 
+const swaggerjdc= require('swagger-jsdoc');
+const swagger_ui= require('swagger-ui-express');
+
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
@@ -16,6 +20,29 @@ app.set("views", path.join(__dirname, "views"));
 app.get("/", (req, res) => {
   res.render("client");
 });
+
+///swagger documentation
+const options = {
+  swaggerDefinition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Url shrtener",
+      version: "1.0.0",
+      description: "API documentation for the for Url Shortner application",
+    },
+    servers: [
+      {
+        url: "http://localhost:2000", 
+      },
+    ],
+  },
+  apis: ["./app.js"], 
+};
+
+const swaggerSpec = swaggerjdc(options);
+
+app.use("/api/doc", swagger_ui.serve, swagger_ui.setup(swaggerSpec));
+
 
 
 
@@ -52,6 +79,16 @@ function authenticateUser(req, res, next) {
 
 
 const secretKey='privatekey';
+/**
+ * @swagger
+ * /postUrl:
+ *   get:
+ *     summary: This API is used for URL storage
+ *     description: This API stores the URL entered by the user to the database
+ *     responses:
+ *       200:
+ *         description: To test the URL post method
+ */
 
 app.post("/postUrl", (req, res) => {
   const theUrl = req.body.theURL;
